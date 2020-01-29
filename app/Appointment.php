03657256ee6b5c661f50150f7c25c3de7bbd0dc7
Ronnie <oduor.ron@gmail.com>
2020-01-29 16:09:32 +0300

    appointments sync done

---
+++ app/Appointment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+ public $table = 'tbl_appointment';
+
+}
--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 ini_set('max_execution_time', 0);
 ini_set('memory_limit', '1024M');
 
+use App\Appointment;
+use App\AppointmentFaces;
 use App\Client;
 use App\ClientFaces;
 
@@ -13,6 +15,7 @@ class SyncController extends Controller
  public function index()
  {
   $this->syncClients();
+  $this->syncAppointments();
  }
 
  public function syncClients()
@@ -21,6 +24,15 @@ class SyncController extends Controller
   $clients              = Client::where('partner_id', 18)->where('id', '>', $max_exisiting_client)->get();
   foreach ($clients as $client) {
    ClientFaces::insert($client->toArray());
+  }
+ }
+
+ public function syncAppointments()
+ {
+  $max_exisiting_appointment = AppointmentFaces::max('id');
+  $appointments              = Appointment::join('tbl_client', 'tbl_appointment.client_id', '=', 'tbl_client.id')->where('id', '>', $max_exisiting_appointment)->where('tbl_client.partner_id', 18)->get();
+  foreach ($appointments as $appointment) {
+   AppointmentFaces::insert($appointment->toArray());
   }
  }
 }
