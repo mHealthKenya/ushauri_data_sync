0a86267777ff2bd1f35eca0c05a98f73ba816f20
Ronnie <oduor.ron@gmail.com>
2020-01-29 17:11:25 +0300

    done with till client outcome

---
+++ app/ClientOutcome.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientOutcome extends Model {
+ public $table = 'tbl_clnt_outcome';
+
+}
--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -10,29 +10,43 @@ use App\AppointmentFaces;
 use App\Client;
 use App\ClientFaces;
 
-class SyncController extends Controller
-{
- public function index()
- {
-  $this->syncClients();
+class SyncController extends Controller {
+ public function index() {
+//   $this->syncUsers();
+  //   $this->syncClients();
   $this->syncAppointments();
+  $this->syncClientOutcomes();
  }
 
- public function syncClients()
- {
-  $max_exisiting_client = ClientFaces::max('id');
-  $clients              = Client::where('partner_id', 18)->where('id', '>', $max_exisiting_client)->get();
+ public function syncUsers() {
+  $max_exisiting_user = UserFaces::max('id') ?? 0;
+  $users              = User::where('partner_id', 18)->where('id', '>', $max_exisiting_user)->get();
+  foreach ($users as $user) {
+   UserFaces::insert($user->toArray());
+  }
+ }
+ public function syncClients() {
+  $max_exisiting_client = ClientFaces::max('id') ?? 0;
+
+  $clients = Client::where('partner_id', 18)->where('id', '>', $max_exisiting_client)->get();
   foreach ($clients as $client) {
    ClientFaces::insert($client->toArray());
   }
  }
 
- public function syncAppointments()
- {
-  $max_exisiting_appointment = AppointmentFaces::max('id');
+ public function syncAppointments() {
+  $max_exisiting_appointment = AppointmentFaces::max('id') ?? 0;
   $appointments              = Appointment::join('tbl_client', 'tbl_appointment.client_id', '=', 'tbl_client.id')->where('id', '>', $max_exisiting_appointment)->where('tbl_client.partner_id', 18)->get();
   foreach ($appointments as $appointment) {
    AppointmentFaces::insert($appointment->toArray());
+  }
+ }
+
+ public function syncClientOutcomes() {
+  $max_exisiting_client_outcome = ClientOutcomeFaces::max('id') ?? 0;
+  $client_outcomes              = ClientOutcome::join('tbl_client', 'tbl_clnt_outcome.client_id', '=', 'tbl_client.id')->where('id', '>', $max_exisiting_client_outcome)->where('tbl_client.partner_id', 18)->get();
+  foreach ($client_outcomes as $client_outcome) {
+   ClientOutcomeFaces::insert($client_outcome->toArray());
   }
  }
 }
